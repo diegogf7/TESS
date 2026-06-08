515695a3dc118b2634f1ff10c9a7385cc869e015
diegogf7 <diego.gaf28@gmail.com>
2026-06-08 11:49:53 -0400

    Changed the star plot that we're looking at now?

--- src/visualize.py
+++ src/visualize.py
@@ -14,7 +14,7 @@ model.eval()
 DATA_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/tess_classification.parquet"
 df = pd.read_parquet(DATA_PATH)
 #getting the first instance of the eclipse data just to plot it for fun
-eclipse = df[df['label'] == 'ECLIPSE'].iloc[0]
+eclipse = df[df['label'] == 'DSCT_BCEP'].iloc[0]
 time = np.array(eclipse['time'])
 flux = np.array(eclipse['flux'])
 flux_normalized = normalize(flux)
@@ -29,9 +29,10 @@ with torch.no_grad():
 predictions_new = predictions.squeeze(0).cpu().numpy()
 grid_flux_patches = grid_flux.reshape(64, 16)
 
+
+#need to look this over claude code helped me write thsi
 masked_patch_indexes = shuffle[0, 16:].cpu().numpy()
 
-#need to look this over
 reconstruction = grid_flux.copy()
 for idx in masked_patch_indexes:
     reconstruction[idx*16:(idx+1)*16] = predictions_new[idx]
@@ -45,7 +46,7 @@ plt.plot(x, reconstruction, label='Reconstruction', color='red', alpha=0.7)
 for idx in masked_patch_indexes:
     plt.axvspan(idx*16, (idx+1)*16, alpha=0.1, color='gray')
 
-plt.title("ECLIPSE Light Curve — MAE Reconstruction")
+plt.title("DSCT_BCEP Light Curve — MAE Reconstruction")
 plt.xlabel("Time Steps")
 plt.ylabel("Normalized Flux")
 plt.legend()
