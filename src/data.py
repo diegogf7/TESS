fixed the normalization with max min

src/data.py
@@ -26,19 +26,24 @@ def resample_to_grid(time, flux, grid_length = 1024): #
     return grid_flux, observed
 
 def normalize(flux, clip_sigma = 6.0):
+    
     median = np.median(flux)
-
     if median == 0:
-        median = 1.0 
+        median = 1.0
+    flux = flux / median
 
-    flux = flux / median #need to scale it around 1
+    mean_absolute_deviation = np.median(np.abs(flux - median))
+    scale_to_use = mean_absolute_deviation * 1.4826
+    if scale_to_use > 0:
+        flux = np.clip(flux, 1.0 - clip_sigma * scale_to_use, 1.0 + clip_sigma * scale_to_use) #doesn't this go above 1 and is not between 0 and 1?
 
-    mad = np.median(np.abs(flux - np.median(flux))) #making sure that we got the std but immune to outliers
-    scale_to_use = mad * 1.4826 #changing to our std
-    if scale_to_use >0:
-        #flux = np.clip(flux, -clip_sigma * scale_to_use, clip_sigma * scale_to_use) #I think this is the error
-        flux = np.clip(flux, 1.0 - clip_sigma * scale_to_use, 1.0 + clip_sigma * scale_to_use )
+    flux_min = flux.min()
+    flux_max = flux.max()
+    if flux_max > flux_min:
+        flux = (flux - flux_min) / (flux_max - flux_min)
     return flux
+
+
 
 
 class LightCurveDataset(Dataset):
@@ -69,8 +74,5 @@ if __name__ == "__main__":
     flux, mask = ds[0]
     print(f"Flux shape: {flux.shape}, dtype: {flux.dtype}")
     print(f"Mask shape: {mask.shape}, sum: {mask.sum()}")
-
-
-
 
 