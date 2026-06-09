b1793e46ac66b200488073bb1b4a5dec5a6d751f
diegogf7 <diego.gaf28@gmail.com>
2026-06-09 10:11:41 -0400

    Needed to use the actual training and validation splits with a proper validation loop

--- src/train.py
+++ src/train.py
@@ -10,11 +10,14 @@ from model import masking_autoencoder
 BATCH_SIZE = 256
 EPOCHS = 200
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-DATA_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/tess_classification.parquet"
+#DATA_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/tess_classification.parquet"
 
+DATA_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/TESS/split/tess_classification_train.parquet"
+VAL_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/TESS/split/tess_classification_val.parquet"
 
 print("Loading dataset...", flush=True)
 dataset = LightCurveDataset(DATA_PATH, grid_length = 1024)
+val_dataset = LightCurveDataset(VAL_PATH, grid_length = 1024)
 print(f"Loaded {len(dataset)} light curves", flush=True)
 dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers=4)
 
@@ -24,6 +27,8 @@ optimizer = torch.optim.AdamW(model.parameters(), lr = 0.0001)
 
 #need to make a schedule so that we get the most changes initially
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = EPOCHS)
+
+val_dataloader = DataLoader(val_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers=4)
 
 criteria = nn.MSELoss()
 
@@ -61,7 +66,33 @@ for epoch in range(EPOCHS):
     average_loss = total_loss / len(dataloader)
     print(f'Epoch {epoch+1}/{EPOCHS} complete — Avg Loss: {average_loss:.4f}', flush=True)
     scheduler.step()
-    torch.save(model.state_dict(), '/orcd/scratch/orcd/006/diegogon/checkpoints/mae_final.pth')
 
-        
+    model.eval()
+
+    val_total_loss = 0
+    with torch.no_grad():
+        for batch_idx, (flux, mask) in enumerate(val_dataloader):
+            flux = flux.to(DEVICE)
+            
+            predictions, shuffle = model(flux)
+
+            targets = flux.reshape(-1, 64, 16)
+
+            masked_indexes = shuffle[:, 16:].unsqueeze(-1).expand(-1, -1, 16)
+            prediction_masked = torch.gather(predictions, 1, masked_indexes)
+            true_masked = torch.gather(targets, 1, masked_indexes)
+            loss = criteria(prediction_masked, true_masked)
+
+            val_total_loss += loss.item()
+            if batch_idx % 10 == 0:
+
+                print(f"  Epoch {epoch+1}, batch {batch_idx}/{len(val_dataloader)}, loss: {loss.item():.4f}", flush=True)
+    
+    average_val_loss = val_total_loss / len(val_dataloader)
+    print(f'Epoch {epoch+1}/{EPOCHS} complete — Avg Loss: {average_val_loss:.4f}', flush=True)
+
+
+
+
+    torch.save(model.state_dict(), '/orcd/scratch/orcd/006/diegogon/checkpoints/mae_final.pth')
 
