f43081a7454c7b7768e65c64d286f864b29f183b
diegogf7 <diego.gaf28@gmail.com>
2026-06-11 15:28:26 -0400

    needed to fix some issues with the validation and the training loops with sizing

--- src/train_disentangle.py
+++ src/train_disentangle.py
@@ -45,7 +45,9 @@ for epoch in range(EPOCHS):
         same_sector_flux = same_sector_flux.to(DEVICE)
         same_sector_mask = same_sector_mask.to(DEVICE)
 
-        flux = flux.unsqueeze(-1)
+        anchor_flux = anchor_flux.unsqueeze(-1)
+        same_star_flux = same_star_flux.unsqueeze(-1)
+        same_sector_flux = same_sector_flux.unsqueeze(-1)
 
         optimizer.zero_grad()
 
@@ -54,7 +56,7 @@ for epoch in range(EPOCHS):
 
         reconstruction, x_physics, x_instruments, x_physics_same_star, x_instrument_same_sector = model(anchor_flux, anchor_mask, same_star_flux, same_star_mask, same_sector_flux, same_sector_mask)
 
-        loss = reconstruction_loss(reconstruction, anchor_flux) + consistency_loss(x_physics, x_physics_same_star) + cross_reconstruction_loss(x_physics, x_instrument_same_sector, model.decoder, anchor_flux)
+        loss = reconstruction_loss(reconstruction, anchor_flux.squeeze(-1)) + consistency_loss(x_physics, x_physics_same_star) + cross_reconstruction_loss(x_physics, x_instrument_same_sector, model.decoder, anchor_flux.squeeze(-1))
 
         loss.backward()
         optimizer.step()
@@ -66,8 +68,7 @@ for epoch in range(EPOCHS):
             print(f"Epoch {epoch+1}/{EPOCHS}, batch {batch_idx}, loss: {loss.item():.6f}")
         
     average_loss = total_loss / len(dataloader)
-
-    print(f"Epoch {epoch+1}/{EPOCHS}, average loss: {average_loss:.6f}, Balanced accuracy: {balanced_accuracy}")
+    print(f"Epoch {epoch+1} / {EPOCHS}, average loss: {average_loss:.6f}")
 
     scheduler.step()
 
@@ -77,7 +78,7 @@ for epoch in range(EPOCHS):
 
     with torch.no_grad():
 
-        for batch_idx, (flux, mask, labels) in enumerate(val_dataloader):
+        for batch_idx, (anchor_flux, anchor_mask, same_star_flux, same_star_mask, same_sector_flux, same_sector_mask) in enumerate(val_dataloader):
             anchor_flux = anchor_flux.to(DEVICE)
             anchor_mask = anchor_mask.to(DEVICE)
             same_star_flux = same_star_flux.to(DEVICE)
@@ -85,25 +86,25 @@ for epoch in range(EPOCHS):
             same_sector_flux = same_sector_flux.to(DEVICE)
             same_sector_mask = same_sector_mask.to(DEVICE)
 
-            flux = flux.unsqueeze(-1)
+            anchor_flux = anchor_flux.unsqueeze(-1)
+            same_star_flux = same_star_flux.unsqueeze(-1)
+            same_sector_flux = same_sector_flux.unsqueeze(-1)
 
-            optimizer.zero_grad()
 
             #forwards process now
             
 
             reconstruction, x_physics, x_instruments, x_physics_same_star, x_instrument_same_sector = model(anchor_flux, anchor_mask, same_star_flux, same_star_mask, same_sector_flux, same_sector_mask)
 
-            loss = reconstruction_loss(reconstruction, anchor_flux) + consistency_loss(x_physics, x_physics_same_star) + cross_reconstruction_loss(x_physics, x_instrument_same_sector, model.decoder, anchor_flux)
+            loss = reconstruction_loss(reconstruction, anchor_flux.squeeze(-1)) + consistency_loss(x_physics, x_physics_same_star) + cross_reconstruction_loss(x_physics, x_instrument_same_sector, model.decoder, anchor_flux.squeeze(-1))
 
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+            val_total_loss += loss.item()
 
             if batch_idx % 10 == 0:
                 print(f"Epoch {epoch+1}/{EPOCHS}, batch {batch_idx}, loss: {loss.item():.6f}")
             
     average_loss_val = val_total_loss / len(val_dataloader)
+    print(f"Epoch {epoch+1} / {EPOCHS}, average loss: {average_loss_val:.6f}")
 
 
 
