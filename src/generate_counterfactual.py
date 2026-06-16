1f955ba23a59d905b4253f7351f508671cbd81a8
diegogf7 <diego.gaf28@gmail.com>
2026-06-16 10:21:42 -0400

    adding a new file to graph a counterfactual to see if it is going to be accurate from flow matching

---
+++ src/generate_counterfactual.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+from data import ClassificationDataset
+from disentangle import DisentanglementModel
