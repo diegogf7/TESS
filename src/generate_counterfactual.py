8e80f13316836cb6c6d1ffb983712f988e873cff
diegogf7 <diego.gaf28@gmail.com>
2026-06-16 10:32:37 -0400

    added the loop for sample

--- src/generate_counterfactual.py
+++ src/generate_counterfactual.py
@@ -28,4 +28,30 @@ maskA = maskA.unsqueeze(0).to(DEVICE)
 fluxB = fluxB.unsqueeze(0).to(DEVICE)
 maskB = fluxB.unsqueeze(0).to(DEVICE)
 
+@torch.no_grad() #need to encode all of the physics and instrument latents
+def encode(flux, mask):
+    z_physics = model.physics_encoder(flux.unsqueeze(-1), mask)
+    z_instrument = model.instrument_encoder(flux.unsqueeze(-1), mask)
+
+    return z_physics, z_instrument
+
+z_physics_A, z_instrument_A = encode(fluxA, maskA)
+
+z_physics_B, z_instrument_B = encode(fluxB, maskB)
+
+#need to use the velocity field and noise to make out light curve now
+
 @torch.no_grad()
+def sample(z_physics, z_instrument, steps = 100):
+    context = torch.cat([z_physics, z_instrument], dim = 1)
+
+    x = torch.randn(context.shape[0], 1024, device = DEVICE)
+    for i in range(steps):
+        t = torch.full((context.shape[0], 1), i / steps, device = DEVICE)
+        v = model.velocity_net(x, t, context)
+
+        x = x + (v * (1.0 / steps))
+    
+    return x.squeeze(0).cpu().numpy()
+
+#
