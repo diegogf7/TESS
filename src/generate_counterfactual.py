d9a8a9fe7f7090d99f9daf2c2781d2bdeeee942f
diegogf7 <diego.gaf28@gmail.com>
2026-06-16 10:26:59 -0400

    updates to counterfactual

--- src/generate_counterfactual.py
+++ src/generate_counterfactual.py
@@ -6,3 +6,26 @@ import matplotlib.pyplot as plt
 
 from data import ClassificationDataset
 from disentangle import DisentanglementModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+DATA_PATH = "/orcd/scratch/orcd/006/diegogon/phyts/TESS/TESS/split/tess_classification_test.parquet"
+CKPT_PATH = "/orcd/scratch/orcd/006/diegogon/checkpoints/disentangle.pth"
+OUT_PATH  = "/orcd/scratch/orcd/006/diegogon/plots/counterfactual.png"
+
+model = DisentanglementModel().to(DEVICE)
+
+model.load_state_dict(torch.load(CKPT_PATH, map_location = DEVICE))
+model.eval()
+
+dataset = ClassificationDataset(DATA_PATH, grid_length = 1024)
+fluxA, maskA, _ = dataset[0]
+fluxB, maskB, _ = dataset[1]
+fluxA = fluxA.unsqueeze(0).to(DEVICE)
+maskA = maskA.unsqueeze(0).to(DEVICE)
+
+fluxB = fluxB.unsqueeze(0).to(DEVICE)
+maskB = fluxB.unsqueeze(0).to(DEVICE)
+
+@torch.no_grad()
